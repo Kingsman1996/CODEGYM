a5fbeba9d7e33b0db8d681e140eda93288c11fa0
Kingsman1996 <trader96non@gmail.com>
2025-01-10 11:36:44 +0700

    Them bai tap Love

---
+++ Module1/Lession9/9-7-Love/Love.js
@@ -0,0 +1,7 @@
+function yes(){
+    alert("<3")
+}
+function no(){
+    document.getElementById("no").style.marginLeft = `${Math.random()*500}px`
+    document.getElementById("no").style.marginTop = `${Math.random()*500}px`
+}
