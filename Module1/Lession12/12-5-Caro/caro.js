6fcdb88cec3d3c5bc41cc8ba9b73c3f94203739f
Kingsman1996 <trader96non@gmail.com>
2025-01-14 20:16:23 +0700

    Them bai tap Caro

---
+++ Module1/Lession12/12-5-Caro/caro.js
@@ -0,0 +1,28 @@
+let array = [];
+let span = "<span>0</span>"
+let rowInput, colInput, value
+for (i = 0; i < 5; i++) {
+    array.push([])
+    for (j = 0; j < 5; j++) {
+        array[i][j] = span;
+    }
+}
+
+function drawCaro() {
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            document.getElementById("caro").innerHTML += array[i][j];
+        }
+        document.getElementById("caro").innerHTML += "<br>"
+    }
+}
+
+
+function changeValue() {
+    rowInput = parseInt(prompt("Enter row")) - 1;
+    colInput = parseInt(prompt("Enter column ")) - 1;
+    value = prompt("Enter new value");
+    document.getElementById("caro").innerHTML = ""
+    array[rowInput][colInput] = `<span>${value}</span>`;
+    drawCaro()
+}
