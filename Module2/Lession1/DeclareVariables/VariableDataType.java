b0fc1913c5d0b27d0ddc39f7697bb5a7abd31b32
Kingsman1996 <trader96non@gmail.com>
2025-02-10 16:01:13 +0700

    Them Bai tap khai bao bien

---
+++ Module2/Lession1/DeclareVariables/VariableDataType.java
@@ -0,0 +1,16 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int i = 100;
+        float f = 200.5f;
+        double d = 200.5;
+        boolean b = true;
+        char c = 'X';
+        String s = "Hà Nam";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
