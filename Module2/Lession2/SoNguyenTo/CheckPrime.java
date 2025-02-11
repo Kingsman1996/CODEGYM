46a32ebaed75071dc636b84b5831f42103ab9374
Kingsman1996 <trader96non@gmail.com>
2025-02-11 08:25:28 +0700

    CheckPrime

---
+++ Module2/Lession2/SoNguyenTo/CheckPrime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a integer:");
+        int number = sc.nextInt();
+        if (number < 2) {
+            System.out.println("Not Prime");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not Prime");
+            }
+        }
+    }
+}
