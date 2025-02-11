09d9f4998f5549a884371d88127b26608b5dbf7d
Kingsman1996 <trader96non@gmail.com>
2025-02-11 08:51:56 +0700

    GreatestCommonFactor

---
+++ Module2/Lession2/UCLN/GreatestCommonFactor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GreatestCommonFactor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter integer a");
+        int a = sc.nextInt();
+        System.out.println("Enter integer b");
+        int b = sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("Invalid numbers");
+        } else {
+            while (a != b) {
+                if (a > b) {
+                    a = a - b;
+                } else {
+                    b = b - a;
+                }
+            }
+            System.out.println("Greatest common factor: " + a);
+        }
+    }
+}
