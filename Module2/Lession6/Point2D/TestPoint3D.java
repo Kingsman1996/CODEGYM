e383cbe867a4960c8bd925cf161d1a2ad1cf5aac
Kingsman1996 <trader96non@gmail.com>
2025-02-17 13:03:01 +0700

    Point2D

---
+++ Module2/Lession6/Point2D/TestPoint3D.java
@@ -0,0 +1,7 @@
+public class TestPoint3D    {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D();
+        point3D.setXYZ(6,7,8);
+        System.out.println(point3D.toString());
+    }
+}
