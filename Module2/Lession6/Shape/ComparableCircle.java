6e06162af5622f78f308308542ff47da1575ef9d
Kingsman1996 <trader96non@gmail.com>
2025-02-18 16:29:27 +0700

    Bai2Lession7

---
+++ Module2/Lession6/Shape/ComparableCircle.java
@@ -0,0 +1,21 @@
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle>{
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean isFilled) {
+        super(radius, color, isFilled);
+    }
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) {
+            return 1;
+        } else if (getRadius() < o.getRadius()) {
+            return -1;
+        }
+        return 0;
+    }
+}
