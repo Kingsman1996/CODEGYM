3d74b3d38fa67d6e1c12213df2c5a0685b6b6456
Kingsman1996 <trader96non@gmail.com>
2025-02-21 09:53:55 +0700

    list đơn giản

---
+++ Module2/Week2/DSA-Danh-sách/List-don-gian/src/test/java/MyListTest.java
@@ -0,0 +1,16 @@
+//import static org.junit.jupiter.api.Assertions.*;
+
+class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> integerList = new MyList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            integerList.add(i+1);
+        }
+        System.out.println("element 1: " + integerList.get(1));
+        System.out.println("element 2: " + integerList.get(2));
+        System.out.println("element 4: " + integerList.get(4));
+        integerList.get(-1);
+        System.out.println("element 6: " + integerList.get(-1));
+    }
+
+}
