ded552f6e1e883ad6118e887333ecf1c08781485
Kingsman1996 <trader96non@gmail.com>
2025-04-24 09:37:38 +0700

    Update

---
+++ Module4/Week2/JPA/thuchanh1/src/main/java/com/thuchanh1/repo/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.thuchanh1.repo;
+
+import com.thuchanh1.model.Customer;
+
+public interface ICustomerRepository extends IGenerateRepository<Customer> {
+}
---
+++ Module4/Week2/JPA/thuchanh1/src/main/java/com/thuchanh1/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.thuchanh1.service;
+
+import com.thuchanh1.model.Customer;
+
+public interface ICustomerService extends IGenerateService<Customer> {
+}
